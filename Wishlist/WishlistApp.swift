5a7b5d9616d0e117d8171594f1399f1419dfde55
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2025-01-27 01:41:21 +0530

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Kshitij Singh Chauhan on 27/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
